594a71236928dc7def18d303131110d96f27a309
Parin Kanthakamala <parint.k@siamraj.com>
2023-09-11 11:29:39 +0700

    temp : save point

--- entities creator/mysql.sh
+++ entities creator/mysql.sh
@@ -1,18 +1,17 @@
+#!/bin/bash
 clear
 
 APP_NAME="${PWD##*/}"
- 
+
 dotnet new webapi --name $APP_NAME --output ./
 dotnet add package Microsoft.EntityFrameworkCore --version 3.1.6
 dotnet add package Microsoft.EntityFrameworkCore.Design --version 3.1.6
 dotnet add package Pomelo.Entityframeworkcore.MySql --version 3.1.2
 
-
 mkdir Entities
- 
-dotnet ef dbcontext scaffold "Server=localhost;User=root;Database=${APP_NAME};Convert Zero Datetime=True" pomelo.entityframeworkcore.mysql -c MyContext -o ./Entities -f 
+
+dotnet ef dbcontext scaffold "Server=localhost;User=root;Database=${APP_NAME};Convert Zero Datetime=True" pomelo.entityframeworkcore.mysql -c MyContext -o ./Entities -f
 dotnet ef migrations add MyFirstMigration
 #dotnet ef database update
 
-
-dotnet ef dbcontext scaffold "DataSource=./storage.sqlite" Microsoft.EntityFrameworkCore.Sqlite -c MyContext -o ./Entities -f 
+dotnet ef dbcontext scaffold "DataSource=./storage.sqlite" Microsoft.EntityFrameworkCore.Sqlite -c MyContext -o ./Entities -f
