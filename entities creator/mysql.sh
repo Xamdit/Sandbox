f7b2213f4eb3e5e318ff8fd2a14c284daaaf751e
Parin Kanthakamala <codelanding@gmail.com>
2024-01-24 16:08:04 +0700

    update : create entities for mysql

--- entities creator/mysql.sh
+++ entities creator/mysql.sh
@@ -7,9 +7,7 @@ dotnet new webapi --name $APP_NAME --output ./
 dotnet add package Microsoft.EntityFrameworkCore --version 3.1.6
 dotnet add package Microsoft.EntityFrameworkCore.Design --version 3.1.6
 dotnet add package MySql.EntityFrameworkCore
-
 mkdir Entities
-
 dotnet ef dbcontext scaffold "Server=localhost;User=root;Database=${APP_NAME};Convert Zero Datetime=True" MySql.EntityFrameworkCore -c MyContext -o ./Entities -f
 # dotnet ef migrations add MyFirstMigration
 #dotnet ef database update
